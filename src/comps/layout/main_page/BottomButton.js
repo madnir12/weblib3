99f90be203b2a7c01c181a9c3bbab679202fcf56
madnir12 <anfz12001@gmail.com>
2023-05-01 23:04:41 +0500

    fixed bottom bar share popup and link box

--- src/comps/layout/main_page/BottomButton.js
+++ src/comps/layout/main_page/BottomButton.js
@@ -2,11 +2,10 @@ import React from 'react'
 import {useLocation,Link} from 'react-router-dom'
 const BottomButton = ({path,icon,text}) => {
   const location = useLocation();
-console.log(location.pathname)
   return (
     <Link to={path}>
     <div className={location.pathname === path ?                        
-      "bottom-button-container active" : "bottom-button-container"  }>
+      "bottom-button-container active" : path === "dashboard" && location.pathname.includes("dashboard") ? "bottom-button-container active" : path === "/" && location.pathname.includes("/book") ? "bottom-button-container active" : "bottom-button-container"  }>
           <span>{icon}</span>
           <span>{text}</span>
     </div>
